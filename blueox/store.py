970905e3f3e7c8657e19a21b135492e12b89ea43
Rhett Garber <rhettg@gmail.com>
2015-05-22 11:32:41 -0700

    LogFile utility class for creating/parsing log file names

--- blueox/store.py
+++ blueox/store.py
@@ -13,6 +13,8 @@ oxd.
 """
 import logging
 import datetime
+import os
+import re
 
 from . import errors
 
@@ -35,3 +37,70 @@ def parse_date_range_argument(value):
             pass
 
     raise InvalidDateError()
+
+
+class LogFile(object):
+    """Represents a remote log file"""
+
+    def __init__(self, type_name, host=None, dt=None, date=None, bzip=False):
+        self.type_name = type_name
+        self.host = host
+        self.bzip = bzip
+        if (dt, date) == (None, None):
+            raise ValueError("Needs a date")
+
+        self.dt = dt
+        self.date = date or dt.date()
+
+    @property
+    def file_path(self):
+        date_str = self.date.strftime('%Y%m%d')
+
+        if self.dt:
+            date_name_str = self.dt.strftime('%Y%m%d%H')
+        else:
+            date_name_str = date_str
+
+        bzip_str = ""
+        if self.bzip:
+            bzip_str = ".bz2"
+
+        host_str = ""
+        if self.host:
+            host_str = "-{}".format(self.host)
+
+        return "{date}/{type}-{date_name}{host_str}.log{bzip}".format(
+            date=date_str,
+            type=self.type_name,
+            host_str=host_str,
+            date_name=date_name_str,
+            bzip=bzip_str)
+
+    @classmethod
+    def from_filename(cls, filename):
+        basename = os.path.basename(filename)
+        match = re.match(
+            r"^(?P<stream>.+)" # stream name like "nginx-error"
+            r"\-(?P<date>\d{8,10})" # date like 20140229 or 2014022910
+            r"\-?(?P<host>.+)?" # optional server name
+            r"\.log" # .log
+            r"(?P<zip>\.bz2|\.gz)?$",
+            basename)
+
+        if match is None:
+            raise ValueError(basename)
+
+        match_info = match.groupdict()
+
+        log_dt = log_date = None
+        if len(match_info['date']) == len('yyyymmddHH'):
+            log_dt = datetime.datetime.strptime(match_info["date"], '%Y%m%d%H')
+        else:
+            log_date = datetime.datetime.strptime(match_info["date"], '%Y%m%d').date()
+
+        return LogFile(
+            match_info["stream"],
+            host=match_info.get('host'),
+            dt=log_dt,
+            date=log_date,
+            bzip=bool(match_info['zip']))
--- tests/store_test.py
+++ tests/store_test.py
@@ -17,3 +17,50 @@ class ParseDateArgumentTestCase(TestCase):
     def test_bad(self):
         with assert_raises(store.InvalidDateError):
             store.parse_date_range_argument("foo")
+
+
+class LogFileFilePathTes(TestCase):
+    def test_simple_date(self):
+        date = datetime.date(2015, 5, 21)
+        lf = store.LogFile("foo", date=date)
+        assert_equal(lf.file_path, "20150521/foo-20150521.log")
+
+    def test_simple_dt(self):
+        dt = datetime.datetime(2015, 5, 21, 20)
+        lf = store.LogFile("foo", dt=dt)
+        assert_equal(lf.file_path, "20150521/foo-2015052120.log")
+
+    def test_dt_host(self):
+        dt = datetime.datetime(2015, 5, 21, 20)
+        lf = store.LogFile("foo", dt=dt, host='localhost')
+        assert_equal(lf.file_path, "20150521/foo-2015052120-localhost.log")
+
+    def test_dt_zipped(self):
+        dt = datetime.datetime(2015, 5, 21, 20)
+        lf = store.LogFile("foo", dt=dt, bzip=True)
+        assert_equal(lf.file_path, "20150521/foo-2015052120.log.bz2")
+
+
+class LogFileFromFilenameTest(TestCase):
+    def test_simple_date(self):
+        file_name = "/var/log/20150521/foo-20150521.log"
+        lf = store.LogFile.from_filename(file_name)
+        assert_equal(lf.type_name, "foo")
+        assert_equal(lf.date, datetime.date(2015, 5, 21))
+        assert_equal(lf.host, None)
+        assert_equal(lf.bzip, False)
+
+    def test_simple_dt(self):
+        file_name = "/var/log/20150521/foo-2015052120.log"
+        lf = store.LogFile.from_filename(file_name)
+        assert_equal(lf.dt, datetime.datetime(2015, 5, 21, 20))
+
+    def test_host(self):
+        file_name = "/var/log/20150521/foo-2015052120-localhost.log"
+        lf = store.LogFile.from_filename(file_name)
+        assert_equal(lf.host, "localhost")
+
+    def test_bzip(self):
+        file_name = "/var/log/20150521/foo-2015052120-localhost.log.bz2"
+        lf = store.LogFile.from_filename(file_name)
+        assert lf.bzip
